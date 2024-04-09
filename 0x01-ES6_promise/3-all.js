d1308aa8287a50a70412bd933994d18034efd643
GLetiwa <graceletiwa@gmail.com>
2024-04-09 16:21:16 +0000

    promises, then and catch

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((responses) => {
-      const [photoResponse, userResponse] = responses;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`);
     })
     .catch(() => {
       console.error('Signup system offline');
