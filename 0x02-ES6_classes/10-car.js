b1ad1229752ccbff14e7f9c06109e78ddef962b1
GLetiwa <graceletiwa@gmail.com>
2024-04-11 11:20:33 +0000

    fix code

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  cloneCar() {
+    
+  }
+}
