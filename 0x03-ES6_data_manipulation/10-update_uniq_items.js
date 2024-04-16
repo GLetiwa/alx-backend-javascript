039f37d684080ccbb9c33d19b1113e9d9a54e7dc
GLetiwa <graceletiwa@gmail.com>
2024-04-16 08:15:27 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 function updateUniqueItems(groceriesMap) {
-    if (!(groceriesMap instanceof Map)) {
-        throw new Error('Cannot process');
-    }
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
-    for (const [key, value] of groceriesMap) {
-        if (value === 1) {
-            groceriesMap.set(key, 100);
-        }
+  for (const [key, value] of groceriesMap) {
+    if (value === 1) {
+      groceriesMap.set(key, 100);
     }
+  }
 }
 
 export default updateUniqueItems;
